cd361958c2541aeeb7ad48390f1282b11e21f2d9
Aquila Peeran <aquilapeeran19@gmail.com>
2021-03-18 12:10:38 +0530

    header made

---
+++ src/navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return ( 
+        
+        <div className="header">
+        <img src="https://ionicframework.com/docs/assets/icons/logo-react-icon.png" alt="React"/>
+        <span>Welcome to BSv Non RPA Portal</span>
+        </div>
+     );
+}
+ 
+export default Navbar;
