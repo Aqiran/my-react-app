09eb7b015b50b9ece2c36bc474cdaa03e1325ede
Aquila Peeran <aquilapeeran19@gmail.com>
2021-03-23 14:14:13 +0530

    toolbar

--- src/navbar.js
+++ src/navbar.js
@@ -1,9 +1,30 @@
 const Navbar = () => {
     return ( 
-        
+        <div>
         <div className="header">
         <img src="https://ionicframework.com/docs/assets/icons/logo-react-icon.png" alt="React"/>
         <span>Welcome to BSv Non RPA Portal</span>
+        </div>
+
+        <div className="toolbar">
+            <a className="links" href="/">
+            <img src="https://cdn3.iconfinder.com/data/icons/universal-27/76/icon-29-512.png" alt="cap"/>HOME
+            </a>
+            
+            <a className="links" href="/">
+            <img src="https://cdn3.iconfinder.com/data/icons/universal-27/76/icon-29-512.png" alt="cap"/>
+            TOOLS
+            </a>
+
+            <a className="links" href="/">
+            <img src="https://cdn3.iconfinder.com/data/icons/universal-27/76/icon-29-512.png" alt="cap"/>ENGAGEMENTS
+            </a>
+
+            <a className="links" href="/">
+            <img src="https://cdn3.iconfinder.com/data/icons/universal-27/76/icon-29-512.png" alt="cap"/>iPORTAL
+            </a>
+        </div>
+        
         </div>
      );
 }
