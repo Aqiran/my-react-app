70c19f1369b0197304afd36d4300b609999794d5
Aquila Peeran <aquilapeeran19@gmail.com>
2021-03-29 08:28:45 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
-import Navbar from './navbar'
-import Login from './login'
+import Navbar from './navbar';
+import Login from './login';
+import Home from './home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar/>
+      <Switch>
+      <Route exact path="/">
       <Login/>
+      </Route>
+        <Route exact path="/home">
+         <Home/>
+        </Route>
+      </Switch>
+      
     </div>
+    </Router>
   );
 }
 
