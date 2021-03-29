ef4b851207c97afab9859a24cc8fca70676d56f3
Aquila Peeran <aquilapeeran19@gmail.com>
2021-03-29 12:17:39 +0530

    tables added

--- src/navbar.js
+++ src/navbar.js
@@ -1,34 +1,36 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
-    return ( 
+    return (
         <div>
-        <div className="header">
-        <img src="https://ionicframework.com/docs/assets/icons/logo-react-icon.png" alt="React"/>
-        <span>Welcome to BSv Non RPA Portal</span>
-        </div>
+            <div className="header">
+                <img src="https://ionicframework.com/docs/assets/icons/logo-react-icon.png" alt="React" />
+                <span>Welcome to BSv Non RPA Portal</span>
+            </div>
 
-        <div className="toolbar">
-            <a className="links" href="/">
-            <img src="https://cdn3.iconfinder.com/data/icons/universal-27/76/icon-29-512.png" alt="cap"/>Home
-            </a>
-            
-            <a className="links" href="/">
-            <img src="https://cdn3.iconfinder.com/data/icons/universal-27/76/icon-29-512.png" alt="cap"/>
+            <div className="toolbar">
+                <Link className="links" to="/home">
+                    <img src="https://cdn3.iconfinder.com/data/icons/universal-27/76/icon-29-512.png" alt="cap" />Home
+            </Link>
+
+                <Link className="links" to="/">
+                    <img src="https://cdn3.iconfinder.com/data/icons/universal-27/76/icon-29-512.png" alt="cap" />
             Tools
-            </a>
+            </Link>
 
-            <a className="links" href="/">
-            <img src="https://cdn3.iconfinder.com/data/icons/universal-27/76/icon-29-512.png" alt="cap"/>Engagements
-            </a>
+                <Link className="links" to="/">
+                    <img src="https://cdn3.iconfinder.com/data/icons/universal-27/76/icon-29-512.png" alt="cap" />Engagements
+            </Link>
 
-            <a className="links" href="/">
-            <img src="https://cdn3.iconfinder.com/data/icons/universal-27/76/icon-29-512.png" alt="cap"/>iPortal
-            </a>
+                <Link className="links" to="/">
+                    <img src="https://cdn3.iconfinder.com/data/icons/universal-27/76/icon-29-512.png" alt="cap" />iPortal
+            </Link>
+            </div>
+
+            <p>Welcome to the BSV Non RPA Tools Portal</p>
+
         </div>
-
-        <p>Welcome to the BSV Non RPA Tools Portal</p>
-        
-        </div>
-     );
+    );
 }
- 
+
 export default Navbar;
