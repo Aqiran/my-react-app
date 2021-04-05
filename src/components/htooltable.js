405d1bf536cd3cad6779e2a3695e5cf69e008c34
Aquila Peeran <aquilapeeran19@gmail.com>
2021-04-05 13:56:43 +0530

    2 tables added with pagination

---
+++ src/components/htooltable.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import {
+  Container,
+  Paper,
+  Box,
+  Typography,
+  TableRow,
+  TableContainer,
+  Table,
+  TableCell,
+  TableBody,
+  TableHead,
+  TablePagination
+} from "@material-ui/core";
+import axios from "axios";
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.action.selected,
+    color: theme.palette.common.black,
+    borderBottomColor: theme.palette.common.black
+  },
+  body: {
+    fontSize: 14
+  }
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover
+    }
+  }
+}))(TableRow);
+
+const useStyles = makeStyles({
+  table: {
+    maxWidth: 500
+  }
+});
+
+export default function ToolTable() {
+  const classes = useStyles();
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const loadUsers = async () => {
+    const res = await axios.get(
+      "https://bsvnonrpa-python.herokuapp.com/api/tools"
+    );
+    setUsers(res.data);
+  };
+
+  const onChangePage = (event, nextPage) => {
+    setPage(nextPage);
+  }; //to go to new page
+
+  const onChangeRowsPerPage = (event) => {
+    setRowsPerPage(event.target.value);
+  };
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+
+  return (
+    <Container>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Tool</StyledTableCell>
+              <StyledTableCell>Process</StyledTableCell>
+              <StyledTableCell>MainFeatures</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((user) => (
+                <StyledTableRow>
+                  <StyledTableCell>{user.ToolName}</StyledTableCell>
+                  <StyledTableCell>{user.Process}</StyledTableCell>
+                  <StyledTableCell>{user.Features}</StyledTableCell>
+                </StyledTableRow>
+              ))}
+          </TableBody>
+        </Table>
+        <TablePagination
+          rowsPerPageOptions={[10, 20, 30]}
+          count={users.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onChangePage={onChangePage}
+          onChangeRowsPerPage={onChangeRowsPerPage}
+        />
+      </TableContainer>
+    </Container>
+  );
+}
--- src/home.js
+++ src/home.js
@@ -1,54 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { Container, Paper, Box, Typography, TableRow, TableContainer, Table, TableCell, TableBody, TableHead } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import axios from "axios";
+import ToolTable from './components/htooltable';
+import EngageTable from './components/hengagetable';
+import './components/home.css'
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-
-        backgroundColor: theme.palette.black,
-        paddingTop: theme.spacing(3),
-        maxWidth: "700px"
-    },
-
-}));
-
-function Home() {
-    const classes = useStyles();
-    const [users, setUsers] = useState([]);
-
-    const loadUsers = async () => {
-        const res = await axios.get("https://bsvnonrpa-python.herokuapp.com/api/tools");
-        setUsers(res.data);
-    };
-
-    useEffect(() => {
-        loadUsers();
-    }, []);
-
+const Home = () => {
     return (
-        <Container className={classes.root}>
-            <TableContainer component={Paper}>
-                <TableHead className={classes.head}>
-                    <TableRow>
-                        <TableCell>Tools</TableCell>
-                        <TableCell>Process</TableCell>
-                        <TableCell>MainFeatures</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        users.map(user => (<TableRow>
-                            <TableCell>{user.ToolName}</TableCell>
-                            <TableCell>{user.Process}</TableCell>
-                            <TableCell>{user.Features}</TableCell>
-                        </TableRow>))
-                    }
-
-                </TableBody>
-            </TableContainer>
-        </Container>
-    );
+        <div className="row">
+            <div className="col">
+                <ToolTable />
+            </div>
+            <div className="col">
+                <EngageTable />
+            </div>
+        </div>);
 }
 
 export default Home;
